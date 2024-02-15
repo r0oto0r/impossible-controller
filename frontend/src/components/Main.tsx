f8df20f6a469d084468a863c29934fc63d6ba314
ben <ben@forkbomb.dev>
2024-02-15 11:59:01 +0100

    Added blank page.

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import KeysPressedView from "./KeysPressedView";
 import LiveLinkVRM from "./LiveLinkVRM";
 import KeyMapping from "./KeyMapping";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import VoiceCommand from "./VoiceCommand";
 
 function Main(): JSX.Element {
 	return (
-		<React.Fragment>
-			<KeysPressedView />
-			<LiveLinkVRM />
-			<KeyMapping />
-		</React.Fragment>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<React.Fragment />} />
+				<Route path="/facelink" element={
+					<React.Fragment>
+						<KeysPressedView />
+						<LiveLinkVRM />
+						<KeyMapping />
+					</React.Fragment>
+				} />
+				<Route path="/voicecommand" element={
+					<React.Fragment>
+						<KeysPressedView />
+						<VoiceCommand />
+					</React.Fragment>
+				} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
---
+++ frontend/src/components/VoiceCommand.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VoiceCommand(): JSX.Element {
+	return (
+		<div>
+			<h1>VoiceCommand</h1>
+		</div>
+	);
+};
+
+export default VoiceCommand;
